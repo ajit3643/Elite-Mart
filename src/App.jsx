fb7ae0398918f6f15c1ed3b59d40f60ceacf2128
Kumar Ajit <ajitinfotech28@gmail.com>
2025-02-03 19:17:34 +0530

    Add login & signup page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import HomePage from "./pages/home/HomePage";
 import NoPage from "./pages/noPage/NoPage";
 import ProductInfo from "./pages/productInfo/ProductInfo";
 import ScrollTop from "./components/scrollTop/ScrollTop";
+import CartPage from "./pages/cart/CartPage";
+import AllProduct from "./pages/allProduct/AllProduct";
+import Signup from "./pages/registration/Signup";
+import Login from "./pages/registration/Login";
 
 const App = () => {
   return (
@@ -13,6 +17,10 @@ const App = () => {
           <Route path="/" element={<HomePage />} />
           <Route path="/*" element={<NoPage />} />
           <Route path="/productinfo" element={<ProductInfo />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/allproduct" element={<AllProduct />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </div>
